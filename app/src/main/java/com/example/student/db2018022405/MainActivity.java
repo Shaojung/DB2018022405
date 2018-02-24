88de7687013664444df8179cbf5647a8a1eb3e0d
Shaojung <shaojung@gmail.com>
2018-02-24 15:01:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2018022405/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.student.db2018022405;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                String str_url = "https://www.mobile01.com/rss/news.xml";
+                URL url = null;
+                StringBuilder sb = new StringBuilder();
+                String str = "";
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream stream = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+                    while ((str = br.readLine()) != null)
+                    {
+                        sb.append(str);
+                    }
+                    br.close();
+                    stream.close();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String data = sb.toString();
+                Log.d("NET", data);
+
+            }
+        }.start();
+    }
+}
